4579f2df2851e8ca4484b7fd730bd0680f8168da
Sandaime-As <asenov1998@mail.ru>
2022-09-21 22:56:36 +0600

    Задача с точками

--- Examples/Example005_IfElse/Program.cs
+++ Examples/Example005_IfElse/Program.cs
@@ -1,12 +1,12 @@
-﻿Console.Write("Введите имя пользователя: "); //хз что тут не работает, надо будет потом глянуть
-string username = Console.ReadLine();
+﻿//Console.Write("Введите имя пользователя: "); //хз что тут не работает, надо будет потом глянуть
+//string username = Console.ReadLine();
 
-if (username == "Маша");
-{
-    Console.WriteLine("Ура, это же МАША!");
-}
-else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(username);
-}
+//if (username == "Маша");
+//{
+//    Console.WriteLine("Ура, это же МАША!");
+//}
+//else
+//{
+//    Console.Write("Привет, ");
+//    Console.WriteLine(username);
+//}
